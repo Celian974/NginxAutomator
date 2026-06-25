1697ba1fbfafee0122a35cb313d716301466f538
Celian974 <183355395+Celian974@users.noreply.github.com>
2026-06-25 13:55:27 +0200

    [FEAT] text color & sleep time

--- src/check_uid.c
+++ src/check_uid.c
@@ -5,16 +5,19 @@ int check_uid()
     int uid = getuid();
 
     printf("Verifying user...\n");
+    sleep(1);
 
     if (uid == 0) {
         printf(COLOR_GREEN);
         printf("[✓] User is root.\n");
         printf(COLOR_RESET);
+        sleep(1);
         return 0;
     } else {
         printf(COLOR_RED);
         printf("[𐄂] User is not root.\n");
         printf(COLOR_RESET);
+        sleep(1);
         return 84;
     }
 }
